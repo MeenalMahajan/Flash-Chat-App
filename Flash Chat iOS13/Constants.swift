ffebc91a6133979e6ab17c9ccfa9a7866eba1195
Meenal Mahajan <meenalmahajan1912@gmail.com>
2024-05-26 17:52:52 +0530

    final commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Apple on 30/04/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Apple on 30/04/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
